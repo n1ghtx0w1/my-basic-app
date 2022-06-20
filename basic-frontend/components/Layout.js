ab5d17535d19a2f5bffedf6937dd3159d4d6866c
n1ghtx0w1 <rhead13@wgu.edu>
2022-06-20 16:58:03 -0500

    start of basic app

---
+++ basic-frontend/components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+import styles from '../styles/Layout.module.css'
+
+export default function Layout({title, keywords, description, children}) {
+  return (
+    <div>
+        <Head>
+         <title>{title}</title>
+         <meta name="description" content={description} />
+         <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      <div className={styles.container}>
+      {children}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+Layout.defaultProps = {
+    title: 'Basic App | by Robert',
+    description: 'Keeping busy',
+    keywords: 'robert, head, information, technology, wgu, bcsia, student, developer',
+}
---
+++ basic-frontend/pages/about.js
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+export default function AboutPage() {
+  return (
+    <Layout title='About Me'>
+        <h1>About</h1>
+        <p>
+          Something to mess around with
+        </p>
+      <p>Version 0.0.5</p>
+      <Link href='/'>Home</Link>
+    </Layout>
+
+  )
+}
