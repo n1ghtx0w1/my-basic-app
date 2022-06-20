1e03ff060e3e725d39e15deeaecf27220aa7776e
n1ghtx0w1 <rhead13@wgu.edu>
2022-06-20 17:29:19 -0500

    added banner

--- basic-frontend/components/Layout.js
+++ basic-frontend/components/Layout.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '../styles/Layout.module.css'
 
 export default function Layout({title, keywords, description, children}) {
@@ -12,6 +13,7 @@ export default function Layout({title, keywords, description, children}) {
          <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      <Showcase />
       <div className={styles.container}>
       {children}
       </div>
---
+++ basic-frontend/components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to Basic App</h1>
+      <h2>Just to Learn</h2>
+    </div>
+  )
+}
